90e29399f6199d0f4e1e5a8af9410574448fa647
Diego Aldo Burlando <diego@dmm888.com>
2014-07-28 11:01:47 +0100

    First Commit

---
+++ NodeRvenMysqlSample/Scripts/appcore.js
@@ -0,0 +1,134 @@
+﻿angular.module('moduleApp129', [])
+    .factory('Customers', function ($http) {              
+        this.getCustomers = function () {          
+			//dataTosend = $.param({ userName : "Diego", password : "password" })  // Jquery method to create parameters
+            var datatoSend = [{ user: 'Diego', password: 'password' }, { user: 'MAria', password: 'marypassword' }];            
+            var config = { headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' } };
+            alert("Diego");
+            alert(JSON.stringify(datatoSend));
+            return $http.post('http://localhost:8080', datatoSend, config);			
+        };
+        this.getCustomersJquery = function () {
+             var datatoSend = [{ user: 'Diego', password: 'password' }, { user: 'MAria', password: 'marypassword' }]
+           return  $.ajax({
+                url: 'http://localhost:8080',
+                data: JSON.stringify(datatoSend),
+                type: 'POST',
+                datatype: "json", // The content we expect from the server
+                contentType: "application/x-www-form-urlencoded", // The type of data we send   this is the default. the other value can be  "application/json; charset=utf-8" but doesn't work in chrome and firefox with cross domain calls
+                // notice that for file upload we can use instead  "multipart/form-data" as contentType
+                success: function (dataret) {                  
+                },
+                error: function (xhr, status, error) {
+                    alert(error)
+                }
+            });
+        };
+        return this;
+    })
+    .controller('CustomerController', ['$scope', 'Customers', function ($scope, Customers) {
+        $scope.customers = [];      
+        $scope.getData = function () { Customers.getCustomers().then(function (dataResponse) {$scope.customers = dataResponse.data;}); }
+        $scope.getDataJqueryAjax = function () {
+            Customers.getCustomersJquery().done(function(data){ // data is the response of jquery call in factory
+                if (data) { // The done function is a callback.
+                    alert(data[0].CustomerID);
+                    $scope.customers = data;
+                    $scope.$apply(); // must be called because Jquery ajax is outside the 
+                    // context that angular doesn't know about (the jQuery ajax call in this case).
+                }
+                else alert('error');
+            });
+        }
+        $scope.getDataJqueryAjaxPromise = function () {
+            Customers.getCustomersJquery().promise() // the jquery syntax for promise.
+                .then(function (data) { // data is the response of jquery call in factory
+                if (data) { // The done function is a callback.
+                   // alert(data[0].CustomerID);
+                    $scope.customers = data;
+                    $scope.$apply(); // must be called because Jquery ajax is outside the 
+                    // context that angular doesn't know about (the jQuery ajax call in this case).
+                }
+                else alert('error');
+                })
+                .then(alert('All data has been read'));
+        }
+
+        $scope.clearData = function () { $scope.customers = []; }
+
+
+       
+
+    }])
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//angular.module('moduleApp129', ['ngAnimate'])
+//    .factory('Customers', function ($http) {
+//        var custs = {};
+//        custs.getCustomers = function (callBackfunc) {
+//            $http({
+//                method: 'GET',
+//                url: '/Home/GetData1',
+//                params: 'limit=3, sort_by=city'
+//            }).success(function (data) {
+//                callBackfunc(data);
+//            }).error(function () {
+//                alert("error");
+//            });
+
+//        };
+//        return custs;
+//    })
+//         .controller('CustomerController', ['$scope', 'Customers', function ($scope, Customers) {
+//             $scope.customers = [];
+//             $scope.getInnerData = function (name) { alert(name); }
+//             $scope.getData = function () {
+//                 Customers.getCustomers(function (dataResponse) {
+//                     $scope.customers = dataResponse;
+//                 });
+//             }
+//             $scope.nametosearch;
+//             $scope.$watch('nametosearch', function (newValue, oldValue) {
+//                 //The callback is on initialization and each time the model changes
+//                 if (newValue === oldValue) { return; }
+//                 // here the code that fires on property change
+//             }, true);
+//             $scope.addCustomer = function () {
+//                 $scope.customers.push({ name: $scope.inputData.name, city: $scope.inputData.city });
+//             };
+//         }])
+//         .controller('OrderController', ['$scope', function ($scope) {
+//             $scope.codeName = "Diego Burlando";
+//         }])
+//         .controller('DetailsController', ['$scope', function ($scope) {
+//             $scope.detailName = "MAria Burlando";
+//         }]);
---
+++ NodeRvenMysqlSample/Server.js
@@ -0,0 +1,94 @@
+﻿// immediately-invoked function expression  No pollution of namespace
+(function DMSERVER () {
+    var http = require('http');
+    var util = require('util');
+    var express = require('express');  // express is installed as a module with the following command line:   npm install express
+    var mysql = require('mysql');
+    var extend = require('util')._extend;
+    var $ = require('jquery'); // npm install jquery
+    var fs = require('fs');
+    var ravendb = require('ravendb'); //https://www.npmjs.org/package/ravendb
+//**********************************************************************************************************************************
+        var connected_users = 0;
+        http.createServer(function (request, response) {  // re
+        var dataToServer = '';
+        var pluto = 'hello';
+        response.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' });
+        request.on('data', function (chunk) {
+            console.log('Request received: ');
+            console.log("The request comes from the following client IP " + request.connection.remoteAddress);
+            console.log('Got the following Data:  ' + chunk);
+            dataToServer = JSON.parse(chunk.toString());           
+        });
+        request.on('end', function () {
+            var allRows = {};
+            var connection = mysql.createConnection({
+                host: 'localhost',
+                user: 'Diego',
+                password: 'Atreius@62'
+            });
+            connection.query("use northwind");
+            var strQuery = "select * from customers";
+            connection.query(strQuery, function (err, rows) {
+                allRows = JSON.stringify(extend({}, rows)); // Simple example to show how to clone an object in nodejs                
+                response.end(allRows); // It must be stringified
+                var db = ravendb('http://localhost:777', 'Northwind');
+                var counter = 0;
+                //Do not use a simple for loop because it doesn't work
+                rows.forEach(
+                                function insertRow(row) {
+                                    row.id = counter.toString();  // we need to add the id prior saving it
+                                    db.saveDocument('Customers', row, function (err, result) {
+                                        if (err) console.error(err);
+                                        //else console.log(result);
+                                    })
+                                    counter++;
+                                }                                
+                            );
+                fs.writeFile("test.json", allRows, function (err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("The file was saved!");
+                    }
+                });
+                            
+                console.log(dataToServer[0].user);
+            });
+        });
+    }).listen(8080, "127.0.0.1", function () { console.log("opened server on %j", this.address()); })
+        .on('connection', function (socket) {
+            console.log('someone connected! ...');
+            connected_users += 1;
+            console.log('Number of Connected Users: ' + connected_users)
+            // connected_users[socket.__fd] = socket.remoteAddress;      
+            socket.on('close', function () {
+                connected_users -= 1;
+                console.log('Number of Connected Users: ' + connected_users)
+            });
+        });   
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
